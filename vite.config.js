b629ab838d8d31c0f81c85defbaba174d84402a0
Faisal Evotech <faisal@evotech.global>
2025-05-25 15:53:19 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./Faisalhs51.github.io",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
